d39f46b146658a109518e3de4245bea58eaa9d91
ljungkeit <ljungkeit@vt.edu>
2026-03-04 12:23:31 -0500

    Initial Commit

---
+++ Lab3/Lab3App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab3App.swift
+//  Lab3
+//
+//  Created by CS3714 on 3/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Lab3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
