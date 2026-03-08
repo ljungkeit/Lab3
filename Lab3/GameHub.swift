e61154d96bedd442e84f11f5572e3811a9b89429
Rayan Charah <rayancharah@vt.edu>
2026-03-08 03:27:38 -0400

    Creating the borrow and return functions inside of GameHub that will change the availability

--- Lab3/GameHub.swift
+++ Lab3/GameHub.swift
@@ -5,22 +5,25 @@
 //  Created by Rayan Adam Charah on 3/4/26.
 //
 import SwiftUI
+import Foundation
 
 @Observable
 class GameHub {
-    
-    
     var games: [Game] = []
     
-    struct GameList: View {
-        let games: [Game]
-
-        var body: some View {
-            List {
-                ForEach(games) { game in
-                    Text(game.name)
-                }
-            }
+    func borrowGame(game: Game)
+    {
+        if let index = games.firstIndex(where: { $0.id == game.id})
+        {
+            games[index].available = false
         }
     }
+    func returnGame(game: Game)
+    {
+        if let index = games.firstIndex(where: { $0.id == game.id})
+        {
+            games[index].available = true
+        }
+    }
+        
 }
